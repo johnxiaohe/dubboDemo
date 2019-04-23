f387df8d2966bb2da2037f7ed8e7949b9368658a
johnxiaohe <john_xiaohe@163.com>
2019-04-23 11:34:55 +0800

    添加注解解释

--- dudemo-provider/src/main/java/com/dubbo/dudemoprovider/DudemoProviderApplication.java
+++ dudemo-provider/src/main/java/com/dubbo/dudemoprovider/DudemoProviderApplication.java
@@ -4,8 +4,11 @@ import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+//开启默认的dubbo设置 这个是必须要有的
 @EnableDubbo
+//@PropertySource("classpath:/spring/dubbo-provider.properties") 如果单独设置dubbo的配置可以添加这个注册 classpath表示是resource目录
 @SpringBootApplication
 public class DudemoProviderApplication {
 
@@ -14,3 +17,4 @@ public class DudemoProviderApplication {
 	}
 
 }
+//开启默认设置
