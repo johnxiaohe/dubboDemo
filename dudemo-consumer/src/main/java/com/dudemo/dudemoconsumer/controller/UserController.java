84c7aad994a4ccce94496e64f25093e7b7534435
johnxiaohe <john_xiaohe@163.com>
2019-04-23 16:52:52 +0800

    添加集群容错负载均衡设置注解

--- dudemo-consumer/src/main/java/com/dudemo/dudemoconsumer/controller/UserController.java
+++ dudemo-consumer/src/main/java/com/dudemo/dudemoconsumer/controller/UserController.java
@@ -3,6 +3,7 @@ package com.dudemo.dudemoconsumer.controller;
 
 import com.dudemo.dudemoiprovider.service.UserInfoISV;
 import org.apache.dubbo.config.annotation.Reference;
+import org.apache.dubbo.validation.MethodValidated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ public class UserController {
     //将服务注册到不同的注册中心，从不同的注册中心取
     //check关闭该服务的启时检查默认为true 如果服务不可用会抛出异常组织spring启动。
     // 如果是false就会关闭该检查这样服务不存在时就不会启动不开了
-    @Reference(version="1.0.0",registry = "registry1",check =false)
+    //retries 设置连接服务失败重连次数，默认为2
+    @Reference(version="1.0.0",registry = "registry1",check =false,retries = 5)
     private UserInfoISV userInfoISV1;
     @Reference(version="1.0.0",registry="registry2",check =false)
     private UserInfoISV userInfoISV2;
--- dudemo-provider/src/main/java/com/dubbo/dudemoprovider/serviceImpl/UserInfoISVImpl.java
+++ dudemo-provider/src/main/java/com/dubbo/dudemoprovider/serviceImpl/UserInfoISVImpl.java
@@ -2,11 +2,13 @@ package com.dubbo.dudemoprovider.serviceImpl;
 
 import com.dudemo.dudemoiprovider.service.UserInfoISV;
 import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.validation.MethodValidated;
 import org.springframework.stereotype.Component;
 
 @Component
 //将服务注册到两个不同的注册中心上
-@Service(version="1.0.0",registry = {"registry1","registry2"})
+//loadbalance 服务级别的 表示负载均衡策略
+@Service(version="1.0.0",registry = {"registry1","registry2"},loadbalance = "roundrobin")
 public class UserInfoISVImpl implements UserInfoISV{
 
 
