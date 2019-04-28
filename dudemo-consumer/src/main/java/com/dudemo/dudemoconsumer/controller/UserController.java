438bdf07ad3a8d0ae3ef78a7eb6b4f069c9c0b82
johnxiaohe <john_xiaohe@163.com>
2019-04-28 17:01:27 +0800

    dubbo示例学习

--- dudemo-consumer/src/main/java/com/dudemo/dudemoconsumer/controller/UserController.java
+++ dudemo-consumer/src/main/java/com/dudemo/dudemoconsumer/controller/UserController.java
@@ -17,10 +17,16 @@ public class UserController {
     // 如果是false就会关闭该检查这样服务不存在时就不会启动不开了
     //retries 设置连接服务失败重连次数，默认为2
     //group  设置调用时那个分组下的服务 设置为*表示任意
-    @Reference(version="1.0.0",registry = "registry1",check =false,retries = 5)
+    //mock 值为路径或者true、false 服务降级 在消费端定义逻辑为stu子集   通常用于服务降级，比如某验权服务，当服务提供方全部挂掉后，客户端不抛出异常，而是通过 Mock 数据返回授权失败。
+    //actives = ,默认为0 每  客户端   并发执行（或占用连接的请求数）个数限制最多为多少
+    //connections = 默认为0 限制客户端服务使用连接最大个数
+    //sticky = true, 粘滞连接 注解属性模式开启  lazy = true, 延迟连接注解属性模式开启
+    @Reference(version="1.0.0",registry = "registry1",check =false,retries = 5,mock = "com.dudemo.dudemoconsumer.**serviceMock")
     private UserInfoISV userInfoISV1;
     //loadBalance注解设置负载均衡 url服务器直连，免注册
     //cache 单独指定结果缓存
+    //url 直接指定调用服务
+    //async 设置消费者异步调用服务 true、false
     @Reference(version="1.0.0",url = "47.100.58.201:20880",registry="registry2",check =false,loadbalance = "roundrobin")
     private UserInfoISV userInfoISV2;
 
--- dudemo-provider/src/main/java/com/dubbo/dudemoprovider/serviceImpl/UserInfoISVImpl.java
+++ dudemo-provider/src/main/java/com/dubbo/dudemoprovider/serviceImpl/UserInfoISVImpl.java
@@ -9,7 +9,12 @@ import org.springframework.stereotype.Component;
 //将服务注册到两个不同的注册中心上 registry
 //loadbalance 服务级别的 表示负载均衡策略
 //protocol多协议暴露服务 注解模式
-@Service(version="1.0.0",registry = {"registry1","registry2"},loadbalance = "roundrobin",protocol = {"dubbo","rmi"})
+//stub 注解方式进行本地存根 具体看官方文档 远程服务后，客户端通常只剩下接口，而实现全在服务器端，但提供方有些时候想在客户端也执行部分逻辑，比如：做 ThreadLocal 缓存，提前验证参数，调用失败后伪造容错数据等等，此时就需要在 API 中带上 Stub，客户端生成 Proxy 实例，会把 Proxy 通过构造函数传给 Stub，然后把 Stub 暴露给用户，Stub 可以决定要不要去调 Proxy。
+//executes = 0 默认为0 并发控制 服务器端   并发执行（或占用线程池线程数）个数限制最多为多少
+//actives = 0 默认为0 每  客户端   并发执行（或占用连接的请求数）个数限制最多为多少  或者可以在客户端限制
+//connections = , 限制客户端服务使用连接最大个数  还是如果客户端和服务端都配置了 客户端配置优先
+//token = true/随机字符自己设置,服务级别的令牌验证
+@Service(version="1.0.0",stub = "com.dubbo.dudemoprovider.serviceStub",registry = {"registry1","registry2"},loadbalance = "roundrobin",protocol = {"dubbo","rmi"})
 public class UserInfoISVImpl implements UserInfoISV{
 
 
